7da27dd1a6e9e99edf1293b452258c8be683add1
OmarMcAdam <gh@mcadam.io>
2025-05-19 12:38:56 -0700

    m: modify build commands #40

---
+++ packages/utils/tsup.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig({
+  entry: [
+    "src/index.ts",
+    "src/typeid.ts",
+    "src/ms.ts",
+    "src/convert.ts",
+    "src/uiColors.ts",
+    "src/storage.ts",
+  ],
+  format: ["cjs", "esm"],
+  dts: true,
+  splitting: false, // Recommended for packages to avoid chunks
+  sourcemap: true,
+  clean: true,
+  treeshake: true,
+  external: [
+    "@potential/tsconfig",
+    "convert-units",
+    "itty-time",
+    "nanoid",
+    "typeid-js",
+    "zod",
+  ],
+});
