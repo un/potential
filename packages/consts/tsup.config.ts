edb7bffa1d4238112d122229d8fe5b82bc5a55c1
OmarMcAdam <gh@mcadam.io>
2025-05-19 12:11:08 -0700

    m: modify build commands #33

--- packages/consts/tsup.config.ts
+++ packages/consts/tsup.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["esm"],
+  format: ["cjs", "esm"],
   dts: true,
   clean: true,
   splitting: false,
   sourcemap: true,
   minify: false,
   treeshake: true,
-}); 
+});
