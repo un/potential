1337f83ff927154cd6425d8f1c2ed3c861180121
OmarMcAdam <gh@mcadam.io>
2025-06-01 17:42:12 +0200

    db: change migration connection string

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,12 +1,24 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 
 if (!process.env.DB_MIGRATION_URL) {
   throw new Error("Missing DB_MIGRATION_URL");
 }
 
-export default {
+const url = new URL(
+  `https://${process.env.DB_HOST}/${process.env.DB_DATABASE}`,
+);
+if (url.host.includes("localhost") || url.host.includes("127.0.0.1")) {
+  url.protocol = "http";
+}
+url.password = process.env.DB_PASSWORD ?? "";
+url.username = process.env.DB_USERNAME ?? "";
+url.searchParams.set("rejectUnathorized", "true");
+
+export default defineConfig({
   schema: "./src/schema/",
   dialect: "mysql",
-  dbCredentials: { url: process.env.DB_MIGRATION_URL },
+  dbCredentials: {
+    url: url.toString(),
+  },
   casing: "snake_case",
-} satisfies Config;
+});
